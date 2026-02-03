222de5f61184bd671e40e0fc617f6d77e73de4f0
AbayReushenov <abay.reushenov@gmail.com>
2026-02-03 12:32:43 +0300

    Added plugin piniaPluginPersistedstate

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,21 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 import 'primeicons/primeicons.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
 
 app.use(PrimeVue, {
     theme: {
         preset: Aura,
-        options: {},
     },
 })
 
